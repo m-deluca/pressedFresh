0ff6f84f51abe493414d995746461320840c79ca
Marco DeLuca <mdeluca@swiftpixel.com>
2016-02-04 09:37:06 -0500

    initial upload of app

---
+++ js/app.js
@@ -0,0 +1,77 @@
+// Ionic Starter App
+
+"use strict";
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var BlogApp = angular.module('BlogApp', ['ionic', 'ngRoute', 'ngSanitize'])
+
+.run(function($ionicPlatform,$rootScope,$location) {
+	//global scope, adding function that achnages the url to /list
+	$rootScope.goHome = function() {
+	$location.path('/list');
+  };
+	
+  $ionicPlatform.ready(function() {
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+      // for form inputs)
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+
+      // Don't remove this line unless you know what you are doing. It stops the viewport
+      // from snapping when text inputs are focused. Ionic handles this internally for
+      // a much nicer keyboard experience.
+      cordova.plugins.Keyboard.disableScroll(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+})
+
+BlogApp.config(['$routeProvider', function($routeProvider){
+	$routeProvider
+		.when('/list',
+		{
+			controller: 'ListController',
+			templateUrl: 'partials/list.html'
+		})
+		.when('/details/:itemId', 
+		//this takes the number and stores it in a variable, that we can access with $routeParams.itemId
+		{
+			controller: 'DetailsController',
+			templateUrl: 'partials/details.html'
+		})
+		.otherwise({redirectTo: '/list'});
+}]);
+
+BlogApp.controller('ListController', ['$scope', '$http', '$ionicLoading', function($scope, $http, $ionicLoading) {
+  $scope.loadBlogs = function() {
+	  $ionicLoading.show();
+  $http.get("https://public-api.wordpress.com/rest/v1.1/freshly-pressed")
+    .success(function(response) {
+    console.log(response); //what are we after
+    $scope.posts = response.posts;
+	$ionicLoading.hide();
+    })
+    .finally(function() {
+       // Stop the ion-refresher from spinning
+       $scope.$broadcast('scroll.refreshComplete');
+     });    
+  };
+  $scope.loadBlogs();
+}]);
+
+BlogApp.controller('DetailsController', ['$scope', '$http', '$routeParams','$ionicLoading', function($scope, $http, $routeParams, $ionicLoading) 
+	{	
+	$ionicLoading.show();	
+	//console.log($routeParams.itemId);
+	$http.get("https://public-api.wordpress.com/rest/v1.1/freshly-pressed")
+	.success(function(response) {
+	$scope.title = response.posts[$routeParams.itemId].title;	
+    $scope.fullPost = response.posts[$routeParams.itemId].content;
+	$ionicLoading.hide();
+	console.log($scope.fullPost);
+		});	
+}]);
+
